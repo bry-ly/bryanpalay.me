f4d4f15b8f361579afb786af120665686770802c
Brylle <bryanpalay119@gmail.com>
2026-01-14 23:56:42 +0800

    add portfolio section components

---
+++ components/portfolio/projects/index.tsx
@@ -0,0 +1,24 @@
+import { CollapsibleList } from "@/components/ui/collapsible-list";
+
+import { PROJECTS } from "@/lib/data/projects";
+import { Panel, PanelHeader, PanelTitle, PanelTitleSup } from "@/components/layout/panel";
+import { ProjectItem } from "./project-item";
+
+export function Projects() {
+  return (
+    <Panel id="projects">
+      <PanelHeader>
+        <PanelTitle>
+          Projects
+          <PanelTitleSup>({PROJECTS.length})</PanelTitleSup>
+        </PanelTitle>
+      </PanelHeader>
+
+      <CollapsibleList
+        items={PROJECTS}
+        max={4}
+        renderItem={(item) => <ProjectItem project={item} />}
+      />
+    </Panel>
+  );
+}
